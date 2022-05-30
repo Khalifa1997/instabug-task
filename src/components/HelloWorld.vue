cdf77aa88a3579cbe0c1e52bd43f8df27152f482
Ahmed <ahmed70000@gmail.com>
2022-05-30 17:16:36 +0200

    Added Global Styling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -51,63 +51,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-* {
-  box-sizing: border-box;
-}
-
-/* Slideshow container */
-.slideshow-container {
-  width: 50%;
-  position: relative;
-}
-
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  /*   position: absolute;
-  bottom: 8px;
-  width: 100%; */
-  text-align: center;
-}
-
-.dot-container {
-  position: absolute;
-  bottom: 10px;
-  left: 0;
-  right: 0;
-  margin-left: auto;
-  margin-right: auto;
-}
-.dot {
-  cursor: pointer;
-  height: 10px;
-  width: 10px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-  &-selected {
-    background-color: #717171;
-  }
-  &:hover {
-    background-color: #717171;
-  }
-}
-
-.fade {
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-
-@keyframes fade {
-  from {
-    opacity: 0.3;
-  }
-  to {
-    opacity: 1;
-  }
-}
+<style>
+@import "../assets/css/SlideShow.scss";
 </style>
