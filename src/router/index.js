afaf1daffe0f46a2e8d4048c0d9af1c98a4c85f6
Ahmed <ahmed70000@gmail.com>
2022-06-01 03:15:16 +0200

    404 complete

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import store from "@/store";
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "../views/LoginView.vue";
 import WelcomeView from "../views/WelcomeView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
+
 const routes = [
   {
     path: "/login",
@@ -17,6 +19,20 @@ const routes = [
     path: "/",
     redirect: "/welcome",
   },
+  {
+    path: "/",
+    redirect: "/welcome",
+  },
+  {
+    path: "/404",
+    name: "404",
+    component: NotFoundView,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+
+    redirect: "/404",
+  },
 ];
 
 const router = createRouter({
@@ -26,7 +42,7 @@ const router = createRouter({
 router.beforeEach(async (to, from, next) => {
   const str = await JSON.parse(JSON.stringify(store.state));
 
-  const routes = ["welcome", "login"];
+  const routes = ["welcome", "login", "404"];
   if (routes.includes(to.name)) {
     if (to.name == "welcome" && !str.authenticated)
       return next({ path: "/login" });
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <img :src="background" class="error-image" />
+    <h1 class="error-text">404 - Page Not Found!</h1>
+    <p class="error-paragraph">
+      Sorry, that page doesn't exist. What would you like to do
+    </p>
+    <router-link :to="'/welcome'"
+      ><button class="error-button">Back to Homepage</button></router-link
+    >
+  </div>
+</template>
+
+<script>
+import background from "../assets/notfound.jpg";
+export default {
+  setup() {
+    return {};
+  },
+  data() {
+    return {
+      background,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.error {
+  &-image {
+    width: 200px;
+    margin-top: 100px;
+  }
+  &-text {
+    font-size: 35px;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #00287a;
+  }
+  &-paragraph {
+    font-family: Arial, Helvetica, sans-serif;
+
+    color: #a6a6a6;
+    font-size: 16px;
+  }
+  &-button {
+    padding: 14px 24px 13px;
+    border-style: solid;
+    border-width: 1px;
+    border-color: #246aff;
+    background-color: #246aff;
+    color: #fff;
+    align-items: center;
+    border-radius: 4px;
+    font-size: 16px;
+    font-weight: 600;
+    cursor: pointer;
+  }
+}
+</style>
